b258baa06b23ce7c13b2a65aed05fc2700fe4261
msedou <korda.daaba@gmail.com>
2024-11-30 17:35:17 -0500

    ternaire retest

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -7,6 +7,11 @@ let userMessage;
 let userType = "subscriber";
 let userCategory;
 
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
+
 if (userRole === "admin") {
     accessLevel = "Full access granted";
 } else if (userRole === "manager") {
@@ -42,3 +47,8 @@ switch (userType) {
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+
+/**
+ * 
+ * 
+ */
