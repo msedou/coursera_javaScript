4e459e939bc67d5323056799885d88b3a546d06a
msedou <korda.daaba@gmail.com>
2024-11-30 17:06:50 -0500

    variables scope test

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -30,3 +30,46 @@ function show(){
     console.log(functionVar); // Throws ReferenceError
     console.log(functionLet); // Throws ReferenceError
     console.log(functionConst); // Throws ReferenceError
+
+    {
+        var blockvar = "fruits";
+        let blocklet ="legume";
+        const blockconst = "electro";
+
+        blockvar = "pomme";
+        blocklet = "carotte";
+        blockconst = "elctronique";
+        console.log(blockvar);
+        console.log(blocklet);
+        console.log(blockconst);
+    }
+
+    blockvar = "mangue";
+    blocklet = "obergine";
+    blockconst = "drone";
+    console.log(blockvar);
+    console.log(blocklet);
+    console.log(blockconst);
+    /**
+     * Variable scope overview:
+
+Global scope: Variables declared outside any block or function have a global scope and are accessible throughout the entire script.
+
+Block scope: Variables declared within curly braces {} have block scope, accessible only within that block.
+
+Function scope: Variables declared within a function have function scope, limited to that function's block.
+
+Variable declaration and initialization:
+
+Used var, let, and const to declare and initialize variables in different scopes: global, block, and function.
+
+Demonstrated the behavior of these variables in respective scopes by accessing them outside their defined scope.
+
+Output and scope analysis:
+
+Global variables were accessible everywhere in the script.
+
+Block-scoped variables (inside {}) had limited accessibility, resulting in ReferenceErrors when accessed outside their blocks.
+
+Function-scoped variables (inside a function) also led to ReferenceErrors when accessed outside the function.
+     */
